f2872ab01fa334843e84efca8245c32d2a5defee
icething <whittiger.98@mail.ru>
2019-02-12 16:55:04 +0300

    del

--- lesson1/zad_6.py
+++
@@ -1,9 +0,0 @@
-'''f_n = open('test_file.txt', 'w')
-f_n.write('сетевое программирование\n сокет\n декоратор')
-f_n.close()
-print(f_n)
-'''
-
-with open ('test_file.txt', encoding='utf-8') as f_n:
-    for el_str in f_n:
-        print(el_str)
